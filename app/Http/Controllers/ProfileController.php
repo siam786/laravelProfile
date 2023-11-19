5d4955156c417e47395fb215377054d25d706936
siam786e <shohrab.hossain36@gmail.com>
2023-11-19 14:18:56 +0600

    add complete code

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+
+    public function index($id){
+
+        $name = "Donal Trump";
+        $age = "75";
+        $data=array([
+            "id"=>$id,
+            "name"=>$name,
+            "age"=>$age
+        ]);
+        $name='access_token';
+        $value='123-XYZ';
+        $minutes=1;
+        $path='/';
+        $domain=$_SERVER['SERVER_NAME'];
+        $secure=false;
+        $httpOnly=true;
+        return response()->json([
+            'status'=>'Success',
+            'data'=>$data,
+        ],200)->cookie($name,$value,$minutes,$path,$domain,$secure,$httpOnly);
+    }
+}
